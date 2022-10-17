cb19b839ce7110e8d8c0f6ac03ec54c3fd64bfa3
Vinicius Lima <vinicius.lima@enviou.com.br>
2022-10-17 19:31:42 -0300

    feature/login-screen-style-imporvement: added the olimpo logo and make the improvement

--- src/views/LoginPage/index.jsx
+++ src/views/LoginPage/index.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
-// import { Redirect } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
+import { LayoutComponents } from "../../components/LayoutComponents/index.jsx"
+import logo from "../../assets/olimpo-logo.png"
 // import { useInfo } from '../../context/UserContext';
 
-export const Login = () => {
-    // const [redirect, setRedirect] = useState(false);
+const Login = () => {
+    const [redirect, setRedirect] = useState(false);
     // const [, setUserInfo] = useInfo()[0];
 
-    const [login, setLogin] = useState({
-        email: '',
-        password: '',
-      });
+      const [email, setEmail] = useState("");
+      const [password, setPassword] = useState("");
 
     const handleLogin = async () => {
-        alert ('Login')
+        window.location.href = '/student-dashboard'
         // const makeLogin = await [Realizar a requisição para a a rota de login e colocar o objeto de login no body] ;
         // setUserInfo(makeLogin.data.data.user);
         // const { token } = await makeLogin.data.data;
@@ -33,22 +34,49 @@ export const Login = () => {
     }
 
     return (
-        <>
-            <h2>Página Login</h2>
+        <LayoutComponents>
+            <form className="base-form">
 
-            <input
-                type="text"
-                onChange={(e) => setLogin({ ...login, email: e.target.value })}
-                placeholder="Digite seu e-mail"
-            />
-            <input
-                type="password"
-                onChange={(e) => setLogin({ ...login, password: e.target.value })}
-                placeholder="Digite sua senha"
-            />
-            <button onClick={handleLogin}>Logar</button>
-        </>
-    );
+                <span className="base-form-logo">
+                    <img src={logo} alt="Logo da Olimpo" />
+                </span>
+
+                <span className="base-form-title"> Faça Login </span>
+
+                <div className="wrap-input">
+                    <input          
+                    className={email !== "" ? "has-val input" : "input"}
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="Digite seu E-mail"></span>
+                </div>
+
+                <div className="wrap-input">
+                    <input
+                    className={password !== "" ? "has-val input" : "input"}
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <span className="focus-input" data-placeholder="Digite sua senha"></span>
+                </div>
+
+                <div className="container-base-form-btn">
+                    <Link className="base-form-btn" to="/student-dashboard">Entrar</Link>
+                </div>
+
+                <div className="text-center">
+                    <span className="txt1">Não possui conta? </span>
+                    <Link className="txt2" to="/choose-role">
+                        Criar conta.
+                    </Link>
+                </div>  
+
+            </form>
+        </LayoutComponents>
+    )
 }
 
 export default Login

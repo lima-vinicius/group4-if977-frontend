04b113828611570eb63dd24d2eea6e66af568d03
Vinicius Lima <vinicius.lima@enviou.com.br>
2022-10-19 16:36:15 -0300

    feature/improvement-all-flow: improvement the pop up card and the college trails flow

--- src/views/CollegeTrailsPage/index.jsx
+++ src/views/CollegeTrailsPage/index.jsx
@@ -1,39 +1,46 @@
 import React from 'react';
 
 const CollegeTrails = () => {
+
+    const collegeTrailsData = [
+        {
+            subject: 'Design'
+        },
+        {
+            subject: 'Administração'
+        },        
+        {
+            subject: 'Programação 1'
+        },
+        {
+            subject: 'Programação 2'
+        },
+        {
+            subject: 'Inglês'
+        },
+        {
+            subject: 'Redes'
+        },
+        {
+            subject: 'Lógica'
+        },
+        {
+            subject: 'TCC'
+        },
+        {
+            subject: 'Algoritmos'
+        },
+    ]
     return (
         <div className='main-student-trails'>
             <div className='header-student-trails'>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Design Sprint</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Design Thinking</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Grafos</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Árvores</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Pilha</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Globalização</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Clean Code</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Condicionais</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>Loop</p>
-                </div>
-                <div className='trails-card'>
-                    <p className='title-trail-card'>UX & UI</p>
-                </div>
+                {
+                    collegeTrailsData.map((item, index)=>(
+                        <div key={index} className='trails-card'>
+                            <p className='title-trail-card'>{item.subject}</p>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
